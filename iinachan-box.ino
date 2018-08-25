2addce1e3f6be9ba7616225ed224cf751b8e9ef6
Riku Murai <riku.murai15@imperial.ac.uk>
2018-08-25 17:36:52 +0900

    Add blinking led

--- iinachan-box.ino
+++ iinachan-box.ino
@@ -1,3 +1,5 @@
+#include <lorawan_client.h>
+
 #include <SHT31.h>
 #include <Arduino.h>
 #include <Wire.h>
@@ -5,14 +7,32 @@
 #define SOUND_SENSOR A0
 #define LIGHT_SENSOR A1
 #define PIR_MOTION_SENSOR 8
+#define LED 4
+
+
 SHT31 sht31 = SHT31();
+LoRaWANClient lorawan;
+bool ledStatus = LOW;
+
 void setup()
 {
     Serial.begin(9600);
     pinMode(PIR_MOTION_SENSOR, INPUT);
+    pinMode(LED, OUTPUT);
+    
     while(!Serial);
     Serial.println("begin...");  
-    sht31.begin();  
+    sht31.begin();
+    Serial.println("LoRaWAN module init");
+
+    // LoRaデバイスへ接続
+    /*
+    if (!lorawan.connect(true)) {
+      Serial.println(" failed to connect. Halt...");
+      for (;;) {};
+    }
+    */
+    Serial.println("\nConnected.\n");
 }
 
 void loop() {
@@ -24,14 +44,17 @@ void loop() {
     sound >>= 5;
     Serial.print("sound: ");
     Serial.println(sound);
+    
     // read light
     int light = analogRead(LIGHT_SENSOR);
     Serial.print("light: ");
     Serial.println(light);
+    
     //read pir
     bool pir = digitalRead(PIR_MOTION_SENSOR);
     Serial.print("pir: ");
     Serial.println(pir);
+    
     // read temp/humidity
     float temp = sht31.getTemperature();
     float hum = sht31.getHumidity();
@@ -43,7 +66,10 @@ void loop() {
     Serial.println("%"); 
     Serial.println();
     delay(500);
-
     
+    digitalWrite(LED, ledStatus);
+    ledStatus = ledStatus == HIGH ? LOW : HIGH;
+
+//    lorawan.sendData("test");
 }
 
